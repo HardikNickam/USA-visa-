971ffa019405769c98514e7a59287ac3e39fcf4a
Hardik Suryakant Nikam <hardiknikam7@gmail.com>
2024-09-19 01:46:05 +0530

    utils,exceptions,logger added

---
+++ demo.py
@@ -0,0 +1,9 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+# try:
+#     a = 22/00
+# except Exception as e:
+#     raise USvisaException(e,sys)
+# logging.info("welcome to custom logger")
